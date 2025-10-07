04175576d8a27038672cfdc62f07b1af847e2182
Beka Dursunov <32161488+thisisbeka@users.noreply.github.com>
2025-10-07 15:14:42 +0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amber: {
+          50: '#fffef7',
+          100: '#fffbea',
+          200: '#fff5c7',
+          300: '#ffee9e',
+          400: '#ffdd52',
+          500: '#ffdd52',
+          600: '#e6c749',
+          700: '#ccb041',
+          800: '#b39938',
+          900: '#99832f',
+        },
+      },
+    },
   },
   plugins: [],
 };
