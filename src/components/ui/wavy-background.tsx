fe345981631fd4a6a89b0723ea818a3844a3d920
Beka Dursunov <32161488+thisisbeka@users.noreply.github.com>
2025-10-23 16:07:12 +0500

    Updated wavy-background.tsx

--- src/components/ui/wavy-background.tsx
+++ src/components/ui/wavy-background.tsx
@@ -75,7 +75,7 @@ export const WavyBackground = ({
       ctx.strokeStyle = waveColors[i % waveColors.length];
       for (x = 0; x < w; x += 5) {
         var y = noise(x / 800, 0.3 * i, nt) * 100;
-        ctx.lineTo(x, y + h * 0.5);
+        ctx.lineTo(x, y + h * 0.3);
       }
       ctx.stroke();
       ctx.closePath();
