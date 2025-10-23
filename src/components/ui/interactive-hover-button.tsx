e5a78db4d4b49b20e8a18488ad3d211b712e949e
Beka Dursunov <32161488+thisisbeka@users.noreply.github.com>
2025-10-23 17:46:03 +0500

    Updated interactive-hover-button.tsx

--- src/components/ui/interactive-hover-button.tsx
+++ src/components/ui/interactive-hover-button.tsx
@@ -20,12 +20,12 @@ const InteractiveHoverButton = React.forwardRef<
       )}
       {...props}
     >
-      <span className="inline-block translate-x-1 transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
+      <span className="inline-block transition-all duration-300 group-hover:opacity-0">
         {text}
       </span>
-      <div className="absolute top-0 z-10 flex h-full w-full translate-x-12 items-center justify-center gap-2 text-primary-foreground opacity-0 transition-all duration-300 group-hover:-translate-x-1 group-hover:opacity-100">
+      <div className="absolute top-0 left-0 z-10 flex h-full w-full items-center justify-center gap-2 text-primary-foreground opacity-0 transition-all duration-300 group-hover:opacity-100">
         <span>{text}</span>
-        <ArrowRight />
+        <ArrowRight className="w-5 h-5" />
       </div>
       <div className="absolute left-[20%] top-[40%] h-2 w-2 scale-0 rounded-lg bg-primary transition-all duration-300 group-hover:left-[0%] group-hover:top-[0%] group-hover:h-full group-hover:w-full group-hover:scale-[1] group-hover:bg-primary"></div>
     </button>
